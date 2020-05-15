578e81fdcdb369ab853f65766035e8658fe7936f
benc <ben.clare2@gmail.com>
2020-05-15 21:36:34 +1000

    playing with unsafe

--- topics/rust/learn_traits_draw/src/main.rs
+++ topics/rust/learn_traits_draw/src/main.rs
@@ -1,5 +1,6 @@
 use learn_traits_draw::{Screen, Button, SelectBox};
 use std::boxed::Box;
+use std::slice;
 
 fn main() {
     println!("Hello, world!");
@@ -12,4 +13,22 @@ fn main() {
     };
 
     screen.run();
+
+    let mut arr = [1, 2, 3];
+
+    let (larr, rarr) = split_at_mut(&mut arr, 1);
+
+    println!("Got: {:?} and {:?}", larr, rarr);
+}
+
+fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = slice.len();
+    let ptr = slice.as_mut_ptr();
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len-mid),
+        )
+    }
 }
