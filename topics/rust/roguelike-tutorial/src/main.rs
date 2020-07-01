ba2e727cc163bc1f0430bb30cf9b4a28f2e2cd59
benc <ben.clare2@gmail.com>
2020-07-01 21:09:31 +1000

    collide with walls

--- topics/rust/roguelike-tutorial/src/main.rs
+++ topics/rust/roguelike-tutorial/src/main.rs
@@ -102,10 +102,15 @@ fn register_components(ecs: &mut World) {
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType>>();
 
     for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(WORLD_WIDTH-1, max(0, pos.x + delta_x));
-        pos.y = min(WORLD_HEIGHT-1, max(0, pos.y + delta_y));
+        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+
+        if map[destination_idx] != TileType::Wall {
+            pos.x = min(WORLD_WIDTH-1, max(0, pos.x + delta_x));
+            pos.y = min(WORLD_HEIGHT-1, max(0, pos.y + delta_y));
+        }
     }
 }
 
