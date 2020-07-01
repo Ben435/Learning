c9d19d771e3b863465c3ed63b74354a070a981d5
benc <ben.clare2@gmail.com>
2020-07-01 20:19:40 +1000

    primitive renderer

--- topics/rust/roguelike-tutorial/src/main.rs
+++ topics/rust/roguelike-tutorial/src/main.rs
@@ -1,11 +1,35 @@
-use rltk::{Rltk,GameState};
+use rltk::{Rltk,GameState,RGB};
+use specs_derive::Component;
+use specs::prelude::*;
 
-struct State{}
+struct State {
+    ecs: World,
+}
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
         ctx.print(1, 1, "Hello Rust World");
+
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
     }
+}
+
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: u16,
+    fg: RGB,
+    bg: RGB,
 }
 
 fn main() {
@@ -15,7 +39,21 @@ fn main() {
         .build()
         .unwrap();
 
-    let gs = State{};
+    let mut gs = State {
+        ecs: World::new(),
+    };
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+
+    gs.ecs
+        .create_entity()
+        .with(Position { x: 40, y: 25 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .build();
 
     rltk::main_loop(ctx, gs).unwrap();
 }
