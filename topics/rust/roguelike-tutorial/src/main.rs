53cbfe895ce6d7894b52e0bb47ecfc501f0b2699
benc <ben.clare2@gmail.com>
2020-07-01 20:01:16 +1000

    init

---
+++ topics/rust/roguelike-tutorial/src/main.rs
@@ -0,0 +1,21 @@
+use rltk::{Rltk,GameState,Console};
+
+struct State{}
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello Rust World");
+    }
+}
+
+fn main() {
+    use rltk::RltkBuilder;
+    let ctx = RltkBuilder::simple80x50()
+        .with_title("Roguelike Tutorial")
+        .build()
+        .unwrap();
+
+    let gs = State{};
+
+    rltk::main_loop(ctx, gs).unwrap();
+}
