0da7c9bbfd87006c4b5d9a828bee01b68af77b35
benc <ben.clare2@gmail.com>
2020-07-01 20:40:49 +1000

    basic player input

--- topics/rust/roguelike-tutorial/src/main.rs
+++ topics/rust/roguelike-tutorial/src/main.rs
@@ -1,6 +1,7 @@
-use rltk::{Rltk,GameState,RGB};
+use rltk::{Rltk,GameState,RGB,VirtualKeyCode};
 use specs_derive::Component;
 use specs::prelude::*;
+use std::cmp::{min,max};
 
 const WORLD_WIDTH: i32 = 80;
 const WORLD_HEIGHT: i32 = 50;
@@ -12,6 +13,7 @@ impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
 
+        player_input(self, ctx);
         self.run_systems();
 
         let positions = self.ecs.read_storage::<Position>();
@@ -109,4 +111,27 @@ fn main() {
         .build();
 
     rltk::main_loop(ctx, gs).unwrap();
+}
+
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(WORLD_WIDTH-1, max(0, pos.x + delta_x));
+        pos.y = min(WORLD_HEIGHT-1, max(0, pos.y + delta_y));
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    match ctx.key {
+        None => {},
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {},
+        }
+    }
 }
