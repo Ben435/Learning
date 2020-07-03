603f4bbc9099cd9976c35b9439c43c6f9a91d2d4
benc <ben.clare2@gmail.com>
2020-07-03 20:09:16 +1000

    tooltips

--- topics/rust/roguelike-tutorial/src/gui.rs
+++ topics/rust/roguelike-tutorial/src/gui.rs
@@ -1,7 +1,8 @@
-use rltk::{RGB,Rltk};
-use crate::components::{Player,CombatStats};
+use rltk::{RGB,Rltk,Point};
+use crate::components::{Player,CombatStats,Position,Name};
 use specs::prelude::*;
 use crate::constants::*;
+use crate::map::Map;
 use crate::gamelog::GameLog;
 
 pub fn draw_ui(ecs: &World, ctx: &mut Rltk) {
@@ -25,5 +26,69 @@ pub fn draw_ui(ecs: &World, ctx: &mut Rltk) {
         y += 1;
     }
 
-    
+    // Draw mouse cursor
+    let mouse_pos = ctx.mouse_pos();
+    ctx.set_bg(mouse_pos.0, mouse_pos.1, RGB::named(rltk::MAGENTA));
+
+    draw_tooltips(ecs, ctx);
+}
+
+fn draw_tooltips(ecs: &World, ctx: &mut Rltk) {
+    let tooltip_fg = RGB::named(rltk::WHITE);
+    let tooltip_bg = RGB::named(rltk::GREY);
+
+    let map = ecs.fetch::<Map>();
+    let names = ecs.read_storage::<Name>();
+    let positions = ecs.read_storage::<Position>();
+
+    let mouse_pos = ctx.mouse_pos();
+    if mouse_pos.0 >= map.width || mouse_pos.1 >= map.height {
+        // Mouse outide of map.
+        return;
+    }
+
+    let mut tooltip : Vec<String> = Vec::new();
+    for (name, position) in (&names, &positions).join() {
+        if position.x == mouse_pos.0 && position.y == mouse_pos.1 {
+            tooltip.push(name.name.to_string());
+        }
+    }
+
+    if !tooltip.is_empty() {
+        let width = tooltip
+            .iter()
+            .map(|s| s.len())
+            .max()
+            .unwrap() as i32 + 3;
+
+        if mouse_pos.0 > WORLD_WIDTH / 2 {
+            let arrow_pos = Point::new(mouse_pos.0 - 2, mouse_pos.1);
+            let left_x = mouse_pos.0 - width;
+            let mut y = mouse_pos.1;
+            for s in tooltip.iter() {
+                ctx.print_color(left_x, y, tooltip_fg, tooltip_bg, s);
+
+                // Pad rest of line.
+                let padding = (width - s.len() as i32) - 1;
+                for i in 0..padding {
+                    ctx.print_color(arrow_pos.x - i, y, tooltip_fg, tooltip_bg, &" ".to_string());
+                }
+                y += 1;
+            }
+            ctx.print_color(arrow_pos.x, arrow_pos.y, tooltip_fg, tooltip_bg, &"->".to_string());
+        } else {
+            let arrow_pos = Point::new(mouse_pos.0 + 1, mouse_pos.1);
+            let left_x = mouse_pos.0 +3;
+            let mut y = mouse_pos.1;
+            for s in tooltip.iter() {
+                ctx.print_color(left_x + 1, y, RGB::named(rltk::WHITE), RGB::named(rltk::GREY), s);
+                let padding = (width - s.len() as i32)-1;
+                for i in 0..padding {
+                    ctx.print_color(arrow_pos.x + 1 + i, y, RGB::named(rltk::WHITE), RGB::named(rltk::GREY), &" ".to_string());
+                }
+                y += 1;
+            }
+            ctx.print_color(arrow_pos.x, arrow_pos.y, RGB::named(rltk::WHITE), RGB::named(rltk::GREY), &"<-".to_string());
+        }
+    }
 }
