6f180774858b657c748328956cb9567061527f51
Ben Clare <ben.clare2@gmail.com>
2020-11-11 00:00:05 +1100

    diffuse material basic setup

--- topics/rust/raytracer/src/main.rs
+++ topics/rust/raytracer/src/main.rs
@@ -5,11 +5,14 @@ mod sphere;
 use sphere::Sphere;
 use futures::executor::block_on;
 use std::f32::consts::PI;
-use image::{Rgba,DynamicImage,GenericImage};
+use image::{Rgba,DynamicImage,GenericImage,Pixel};
+use cgmath::{prelude::*,Vector3,vec3};
 
 const WIDTH: u32 = 640;
 const HEIGHT: u32 = 480;
-const NUMS_PER_PIXEL: u32 = 4; // rgba
+
+// const WIDTH: u32 = 64;
+// const HEIGHT: u32 = 48;
 
 pub struct Scene {
     spheres: Vec<Sphere>
@@ -32,14 +35,82 @@ impl Scene {
 
         let aspect_ratio = fwidth / fheight;
         let angle =(PI * 0.5 * fov / 180.0).tan();
+        
+        let iter_coords = (0..width)
+            .into_iter()
+            .flat_map(|x| (0..height)
+                .into_iter()
+                .map(move |y| (x.clone(), y))
+            );
+        for (x, y) in iter_coords {
+            let xx = (2.0 * ((x as f32 + 0.5) * inv_width) - 1.0) * angle * aspect_ratio;
+            let yy = (1.0 - 2.0 * ((y as f32 + 0.5) * inv_height)) * angle;
 
-        for x in 0..width {
-            for y in 0..height {
-                img.put_pixel(x, y, Rgba([128, 255, 128, 255]));
-            }
+            // TODO: Ray origin === eye position
+            let ray_origin = vec3(0.0, 0.0, 0.0);
+            let ray_direction = vec3(xx, yy, -1.0).normalize();
+
+            let color = self.trace(ray_origin, ray_direction, 0);
+
+            img.put_pixel(x, y, color);
         }
 
         img
+    }
+
+    fn trace(&self, ray_origin: Vector3<f32>, ray_direction: Vector3<f32>, current_depth: u8) -> Rgba<u8> {
+        // TODO: This can be reduced by pre-sorting spheres, maybe do later as a pre-processing step.
+        let maybe_intersect = self.spheres.iter().fold(None, |prev, sphere| {
+            let intersection_result = sphere.intersect(ray_origin, ray_direction);
+
+            match intersection_result {
+                None => prev,
+                Some(closest_dist) => {
+                    match prev {
+                        None => Some((closest_dist, sphere)),
+                        Some((min_dist, _s)) => {
+                            if closest_dist < min_dist {
+                                return Some((closest_dist, sphere))
+                            }
+                            prev
+                        }
+                    }
+                },
+            }
+        });
+
+        match maybe_intersect {
+            None => Rgba([0, 0, 0, 255]), // Missed scene, background goes here.
+            Some((dist, sphere)) => {
+                let point = ray_origin + ray_direction * dist;
+                let normal = (point - sphere.origin.to_vec()).normalize();
+
+                // TODO: Handle being inside an object
+
+                let resultant_color: Rgba<u8> = self.spheres.iter()
+                    .filter(|s| s.emmission_color.is_some())
+                    .fold(Rgba([0, 0, 0, 255]), |result_color, light| {
+                        let shadow_ray_origin = point + normal * 1e-4;
+                        let shadow_ray_direction = (light.origin.to_vec() - point).normalize();
+                        let in_shadow = self.spheres.iter()
+                            .filter(|s| s.origin != light.origin)
+                            .find(|s| s.intersect(shadow_ray_origin, shadow_ray_direction).is_some())
+                            .is_some();
+                        
+                        let transmission = match in_shadow {
+                            true => 0.0,
+                            false => 1.0,
+                        };
+
+                        let addition_from_light = sphere.surface_color
+                            .map(|pxl| (pxl as f32 * transmission) as u8) // This is kinda bad, casting like this is messy at best, don't like, may fix
+                            .map2(&light.emmission_color.unwrap(), |pxl1, pxl2| pxl1.saturating_mul(pxl2));
+
+                        return result_color.map2(&addition_from_light, |px1, px2| px1.saturating_add(px2));
+                    });
+                resultant_color
+            }
+        }
     }
 }
 
@@ -47,7 +118,10 @@ fn main() {
     env_logger::init();
     const FOV: f32 = 30.0;
 
-    let spheres = vec![Sphere::new(0.0, 0.0, 0.0, 5.0)];
+    let spheres = vec![
+        Sphere::new(0.0, 0.0, -20.0, 4.0, Rgba([128, 255, 128, 255]), None),
+        Sphere::new(0.0, 20.0, -30.0, 3.0, Rgba([255, 255, 255, 255]), Some(Rgba([255, 255, 255, 255]))),
+    ];
     let scene = Scene::new(spheres);
 
     let img = scene.render(WIDTH, HEIGHT, FOV);
--- topics/rust/raytracer/src/sphere.rs
+++ topics/rust/raytracer/src/sphere.rs
@@ -1,15 +1,51 @@
-use cgmath::Point3;
+use cgmath::{prelude::*,Point3,Vector3};
+use image::Rgba;
 
+#[derive(PartialEq)]
 pub struct Sphere {
-    origin: Point3<f32>,
-    radius: f32,
+    pub origin: Point3<f32>,
+    pub radius: f32,
+
+    pub surface_color: Rgba<u8>,
+    pub emmission_color: Option<Rgba<u8>>
 }
 
 impl Sphere {
-    pub fn new(x: f32, y: f32, z: f32, radius: f32) -> Self {
+    // TODO: Builder
+    pub fn new(x: f32, y: f32, z: f32, radius: f32, surface_color: Rgba<u8>, emmission_color: Option<Rgba<u8>>) -> Self {
         Sphere {
             origin: Point3::new(x, y, z),
             radius,
+            surface_color,
+            emmission_color,
         }
+    }
+
+    /// Returns closest point to ray, or None if no collision
+    pub fn intersect(&self, ray_origin: Vector3<f32>, ray_direction: Vector3<f32>) -> Option<f32> {
+        let length: Vector3<f32> = (self.origin - ray_origin).to_vec();
+
+        // TODO: This can be a member var, save some calcs
+        let radius2: f32 = self.radius * self.radius;
+
+        let tca = length.dot(ray_direction);
+        if tca < 0.0 {
+            return None;
+        }
+        let d2 = length.dot(length) - tca * tca;
+        if d2 > radius2 {
+            return None;
+        }
+
+        let thc = (radius2 - d2).sqrt();
+
+        let t1 = tca + thc;
+        if thc > tca {
+            return Some(t1);
+        }
+
+        let t0 = tca - thc;
+        return Some(t0.min(t1));
+
     }
 }
