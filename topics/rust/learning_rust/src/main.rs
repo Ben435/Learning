629a151e839681250ac5268f850071056a7b6c20
Ben Clare <ben.clare2@gmail.com>
2020-11-17 14:00:11 +1100

    great example of ? notation

--- topics/rust/learning_rust/src/main.rs
+++ topics/rust/learning_rust/src/main.rs
@@ -1,17 +1,51 @@
-fn main() {
-    let sentence = "hello world";
+fn main() -> () {
+    let e1 = Effect::A1;
+    let e2 = Effect::A2;
+    let e3 = Effect::B;
 
-    let word = first_word(&sentence);
-
-    println!("Got: '{}' -> '{}'", sentence, word);
+    do_this(e1).unwrap();
+    do_this(e2).unwrap();
+    do_this(e3).unwrap();
 }
 
-fn first_word(string: &str) -> &str {
-    for (i, &s) in string.as_bytes().iter().enumerate() {
-        if s == b' ' {
-            return &string[..i];
-        }
-    }
+pub enum Effect {
+    A1,
+    A2,
+    B
+}
 
-    return &string[..];
+enum AError {}
+enum BError {}
+#[derive(Debug)]
+pub enum PublicError {}
+
+impl From<AError> for PublicError {
+    fn from(e: AError) -> PublicError {
+        PublicError{}
+    }
+}
+
+impl From<BError> for PublicError {
+    fn from(e: BError) -> PublicError {
+        PublicError{}
+    }
+}
+
+fn doA1() -> Result<(), AError> {
+    Ok(())
+}
+fn doA2() -> Result<(), AError> {
+    Ok(())
+}
+fn doB() -> Result<(), AError> {
+    Ok(())
+}
+
+pub fn do_this(e: Effect) -> Result<(), PublicError> {
+    match e {
+        Effect::A1 => doA1()?,
+        Effect::A2 => doA2()?,
+        Effect::B => doB()?,
+    };
+    Ok(())
 }
