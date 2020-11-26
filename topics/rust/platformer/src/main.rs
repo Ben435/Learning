6aafc485b5364763c842b3e9f5f4a3bbf912ee91
Ben Clare <ben.clare2@gmail.com>
2020-11-26 22:00:51 +1100

    texture

--- topics/rust/platformer/src/main.rs
+++ topics/rust/platformer/src/main.rs
@@ -22,16 +22,17 @@ use wgpu::{
 };
 use futures::executor::block_on;
 use log::error;
+use image::GenericImageView;
 
 const WIDTH: u32 = 640;
 const HEIGHT: u32 = 480;
 
 const VERTICES: &[Vertex] = &[
-    Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.0, 0.5] },
-    Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.5] },
-    Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] },
-    Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 0.5] },
-    Vertex { position: [0.44147372, 0.2347359, 0.0],color: [0.5, 0.0, 0.5] },
+    Vertex { position: [-0.0868241, 0.49240386, 0.0], tex_coords: [0.4131759, 0.00759614], },
+    Vertex { position: [-0.49513406, 0.06958647, 0.0], tex_coords: [0.0048659444, 0.43041354], },
+    Vertex { position: [-0.21918549, -0.44939706, 0.0], tex_coords: [0.28081453, 0.949397057], },
+    Vertex { position: [0.35966998, -0.3473291, 0.0], tex_coords: [0.85967, 0.84732911], },
+    Vertex { position: [0.44147372, 0.2347359, 0.0], tex_coords: [0.9414737, 0.2652641], },
 ];
 
 const INDICES: &[u16] = &[
@@ -50,10 +51,11 @@ struct State {
     render_pipeline: wgpu::RenderPipeline,
 
     vertex_buffer: wgpu::Buffer,
-    vertex_buffer_len: u32,
 
     index_buffer: wgpu::Buffer,
     index_buffer_len: u32,
+
+    diffuse_bind_group: wgpu::BindGroup,
     // camera: Camera,
 }
 
@@ -97,12 +99,103 @@ impl State {
     
         let swap_chain = device.create_swap_chain(&surface, &sc_desc);
 
+        let diffuse_bytes = include_bytes!("./assets/happy-tree.png");
+        let diffuse_image = image::load_from_memory(diffuse_bytes).unwrap();
+        let diffuse_rgba = diffuse_image.as_rgba8().unwrap();
+        let dimensions = diffuse_image.dimensions();
+
+        let texture_size = wgpu::Extent3d {
+            width: dimensions.0,
+            height: dimensions.1,
+            depth: 1,
+        };
+
+        let diffuse_texture = device.create_texture(
+            &wgpu::TextureDescriptor {
+                size: texture_size,
+                mip_level_count: 1,
+                sample_count: 1,
+                dimension: wgpu::TextureDimension::D2,
+                format: wgpu::TextureFormat::Rgba8UnormSrgb,
+                usage: wgpu::TextureUsage::SAMPLED | wgpu::TextureUsage::COPY_DST,
+                label: Some("Saturn Texture")
+            }
+        );
+
+        queue.write_texture(
+            wgpu::TextureCopyView {
+                texture: &diffuse_texture,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+            },
+            diffuse_rgba,
+            wgpu::TextureDataLayout {
+                offset: 0,
+                bytes_per_row: 4 * dimensions.0,
+                rows_per_image: dimensions.1,
+            },
+            texture_size,
+        );
+
+        let diffuse_texture_view = diffuse_texture.create_view(&wgpu::TextureViewDescriptor::default());
+        let diffuse_sampler = device.create_sampler(&wgpu::SamplerDescriptor {
+            address_mode_u: wgpu::AddressMode::ClampToEdge,
+            address_mode_v: wgpu::AddressMode::ClampToEdge,
+            address_mode_w: wgpu::AddressMode::ClampToEdge,
+            mag_filter: wgpu::FilterMode::Linear,
+            min_filter: wgpu::FilterMode::Nearest,
+            mipmap_filter: wgpu::FilterMode::Nearest,
+            ..Default::default()
+        });
+
+        let texture_bind_group_layout = device.create_bind_group_layout(
+            &wgpu::BindGroupLayoutDescriptor {
+                entries: &[
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStage::FRAGMENT,
+                        ty: wgpu::BindingType::SampledTexture {
+                            multisampled: false,
+                            dimension: wgpu::TextureViewDimension::D2,
+                            component_type: wgpu::TextureComponentType::Uint,
+                        },
+                        count: None,
+                    },
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 1,
+                        visibility: wgpu::ShaderStage::FRAGMENT,
+                        ty: wgpu::BindingType::Sampler {
+                            comparison: false,
+                        },
+                        count: None,
+                    }
+                ],
+                label: Some("Saturn Texture Bind Layout")
+            }
+        );
+
+        let diffuse_bind_group = device.create_bind_group(
+            &wgpu::BindGroupDescriptor {
+                layout: &texture_bind_group_layout,
+                entries: &[
+                    wgpu::BindGroupEntry {
+                        binding: 0,
+                        resource: wgpu::BindingResource::TextureView(&diffuse_texture_view),
+                    },
+                    wgpu::BindGroupEntry {
+                        binding: 1,
+                        resource: wgpu::BindingResource::Sampler(&diffuse_sampler),
+                    },
+                ],
+                label: Some("Saturn Texture Bind Group")
+            }
+        );
+
         let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("Vertex Buffer"),
             contents: bytemuck::cast_slice(VERTICES),
             usage: wgpu::BufferUsage::VERTEX,
         });
-        let vertex_buffer_len = VERTICES.len() as u32;
 
         let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("Index Buffer"),
@@ -111,18 +204,45 @@ impl State {
         });
         let index_buffer_len = INDICES.len() as u32;
 
-        let vs_module = device.create_shader_module(include_spirv!("./assets/shader.vert.spv"));
-        let fs_module = device.create_shader_module(include_spirv!("./assets/shader.frag.spv"));
+        let vs_module = device.create_shader_module(include_spirv!("./assets/shaders/shader.vert.spv"));
+        let fs_module = device.create_shader_module(include_spirv!("./assets/shaders/shader.frag.spv"));
 
+        let render_pipeline = State::create_pipeline(&device, &sc_desc, vs_module, fs_module, texture_bind_group_layout);
+
+        // let camera = Camera::new();
+
+        State {
+            surface,
+            device,
+            queue,
+            sc_desc,
+            swap_chain,
+            size,
+            vertex_buffer,
+            index_buffer,
+            index_buffer_len,
+            render_pipeline,
+            diffuse_bind_group,
+            // camera,
+        }
+    }
+
+    fn create_pipeline(
+        device: &wgpu::Device,
+        sc_desc: &wgpu::SwapChainDescriptor,
+        vs_module: wgpu::ShaderModule,
+        fs_module: wgpu::ShaderModule,
+        texture_bind_group_layout: wgpu::BindGroupLayout,
+    ) -> wgpu::RenderPipeline {
         let render_pipeline_layout = device.create_pipeline_layout(
             &wgpu::PipelineLayoutDescriptor {
                 label: Some("Render Pipeline Layout"),
-                bind_group_layouts: &[],
+                bind_group_layouts: &[&texture_bind_group_layout],
                 push_constant_ranges: &[],
             }
         );
 
-        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
             label: Some("Render Pipeline"),
             layout: Some(&render_pipeline_layout),
             vertex_stage: wgpu::ProgrammableStageDescriptor {
@@ -162,24 +282,7 @@ impl State {
             sample_count: 1,
             sample_mask: !0,
             alpha_to_coverage_enabled: false,
-        });
-
-        // let camera = Camera::new();
-
-        State {
-            surface,
-            device,
-            queue,
-            sc_desc,
-            swap_chain,
-            size,
-            vertex_buffer,
-            vertex_buffer_len,
-            index_buffer,
-            index_buffer_len,
-            render_pipeline,
-            // camera,
-        }
+        })
     }
 
     fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
@@ -226,6 +329,7 @@ impl State {
             });
 
             render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.set_bind_group(0, &self.diffuse_bind_group, &[]);
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
             render_pass.set_index_buffer(self.index_buffer.slice(..));
             render_pass.draw_indexed(0..self.index_buffer_len, 0, 0..1);
--- topics/rust/platformer/src/vertex.rs
+++ topics/rust/platformer/src/vertex.rs
@@ -2,7 +2,7 @@
 #[derive(Copy,Clone,Debug,bytemuck::Pod,bytemuck::Zeroable)]
 pub struct Vertex {
     pub position: [f32; 3],
-    pub color: [f32; 3],
+    pub tex_coords: [f32; 2],
 }
 
 impl Vertex {
@@ -19,7 +19,7 @@ impl Vertex {
                 wgpu::VertexAttributeDescriptor {
                     offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
                     shader_location: 1,
-                    format: wgpu::VertexFormat::Float3,
+                    format: wgpu::VertexFormat::Float2,
                 }
             ]
         }
