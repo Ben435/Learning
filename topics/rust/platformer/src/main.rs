2d63e7a5733e8d9369bbde92e03797eaaa95da09
Ben Clare <ben.clare2@gmail.com>
2020-11-26 21:01:39 +1100

    indexed draw

--- topics/rust/platformer/src/main.rs
+++ topics/rust/platformer/src/main.rs
@@ -27,15 +27,17 @@ const WIDTH: u32 = 640;
 const HEIGHT: u32 = 480;
 
 const VERTICES: &[Vertex] = &[
-    Vertex { position: [0.0, 0.5, 0.0], color: [1.0, 0.0, 0.0] },
-    Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0]},
-    Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0]},
+    Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.0, 0.5] },
+    Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.5] },
+    Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] },
+    Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 0.5] },
+    Vertex { position: [0.44147372, 0.2347359, 0.0],color: [0.5, 0.0, 0.5] },
 ];
 
-const INDICES: &[u32] = &[
-    0,
-    1,
-    2
+const INDICES: &[u16] = &[
+    0, 1, 4,
+    1, 2, 4,
+    2, 3, 4,
 ];
 
 struct State {
@@ -46,9 +48,12 @@ struct State {
     swap_chain: wgpu::SwapChain,
     size: winit::dpi::PhysicalSize<u32>,
     render_pipeline: wgpu::RenderPipeline,
+
     vertex_buffer: wgpu::Buffer,
     vertex_buffer_len: u32,
+
     index_buffer: wgpu::Buffer,
+    index_buffer_len: u32,
     // camera: Camera,
 }
 
@@ -171,6 +176,7 @@ impl State {
             vertex_buffer,
             vertex_buffer_len,
             index_buffer,
+            index_buffer_len,
             render_pipeline,
             // camera,
         }
@@ -221,7 +227,8 @@ impl State {
 
             render_pass.set_pipeline(&self.render_pipeline);
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-            render_pass.draw(0..self.vertex_buffer_len, 0..1);
+            render_pass.set_index_buffer(self.index_buffer.slice(..));
+            render_pass.draw_indexed(0..self.index_buffer_len, 0, 0..1);
         }
 
         self.queue.submit(std::iter::once(encoder.finish()));
