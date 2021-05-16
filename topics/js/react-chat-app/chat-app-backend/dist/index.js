5034362d539446ca62c0f6b8a49b335e37c795a5
Ben Clare <ben.clare2@gmail.com>
2021-05-16 10:11:27 +1000

    feat: init

---
+++ topics/js/react-chat-app/chat-app-backend/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = void 0;
+function handler(event) {
+    return '{ "hello": "world" }';
+}
+exports.handler = handler;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi93b3JrZXJzL3dzLWFwaS1jb25uZWN0L2luZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUNBLFNBQWdCLE9BQU8sQ0FBQyxLQUFVO0lBQzlCLE9BQU8sc0JBQXNCLENBQUE7QUFDakMsQ0FBQztBQUZELDBCQUVDIiwic291cmNlc0NvbnRlbnQiOlsiXG5leHBvcnQgZnVuY3Rpb24gaGFuZGxlcihldmVudDogYW55KTogYW55IHtcbiAgICByZXR1cm4gJ3sgXCJoZWxsb1wiOiBcIndvcmxkXCIgfSdcbn1cbiJdfQ==
